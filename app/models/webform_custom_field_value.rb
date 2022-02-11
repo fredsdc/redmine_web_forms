9b1008b53466b9989b3003302a70e55f34229e8a
Frederico Camara <frederico.camara@serpro.gov.br>
2022-02-11 12:18:03 -0300

    rename functions custom fields numbered and positioned implemented identifiers custom_fields in webform

--- app/models/webform_custom_field_value.rb
+++ app/models/webform_custom_field_value.rb
@@ -4,6 +4,10 @@ class WebformCustomFieldValue < ActiveRecord::Base
   belongs_to :webform
   belongs_to :custom_field, lambda { where :type => "IssueCustomField" }
 
+  acts_as_positioned
+
+  scope :sorted, lambda { order(:position) }
+
   validates :custom_field_id, :numericality => { :only_integer => true, :greater_than_or_equal_to => -5, :other_than => 0, :message => :invalid }, :allow_blank => true
   validates_presence_of :custom_field, :if => lambda { custom_field_id.to_i > 0 }
   validates_format_of :identifier, :with => /\A(?!\d+$)[a-z0-9\-_]*\z/
@@ -12,6 +16,7 @@ class WebformCustomFieldValue < ActiveRecord::Base
   safe_attributes(
     'custom_field_id',
     'identifier',
+    'position',
     'default_value'
   )
 
--- db/migrate/20220112142458_create_webform_custom_field_values.rb
+++ db/migrate/20220112142458_create_webform_custom_field_values.rb
@@ -2,8 +2,9 @@ class CreateWebformCustomFieldValues < ActiveRecord::Migration[5.2]
   def change
     create_table :webform_custom_field_values do |t|
       t.integer :webform_id, :null => false
-      t.integer :custom_field_id, :null => false
+      t.integer :custom_field_id
       t.text :identifier
+      t.integer :position, :default => nil, :null => true
       t.text :default_value
     end
     add_index :webform_custom_field_values, [:webform_id, :custom_field_id], :unique => true, :name => :webform_custom_field_values_ids
