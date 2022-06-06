a3ef125a385c8f78aa7671cffde793020c8a2fc1
Frederico Camara <frederico.camara@serpro.gov.br>
2022-06-06 14:22:10 -0300

    Dup @user as roles_for_project sets a class variable

--- app/models/webform.rb
+++ app/models/webform.rb
@@ -51,23 +51,23 @@ class Webform < ActiveRecord::Base
     'identifier')
 
   def validate_webform(user=User.current)
-      roles = (
-        (user.present? ? user.roles_for_project(self.project) : []) |
-        (self.role.present? ? [ self.role ] : []) |
-        Member.where(user_id: self.group_id, project_id: self.project_id).map{|m| m.roles}
-      ).flatten.uniq
+    roles = (
+      (user.present? ? user.dup.roles_for_project(self.project) : []) |
+      (self.role.present? ? [ self.role ] : []) |
+      Member.where(user_id: self.group_id, project_id: self.project_id).map{|m| m.roles}
+    ).flatten.uniq
 
-      self.project.present? &&
-      self.tracker.present? &&
-      self.project.trackers.include?(self.tracker) &&
-      self.issue_status.present? &&
-      roles.map{|role| can_add_issue(role)}.any? &&
-      WorkflowTransition.where(
-        old_status_id: 0,
-        tracker_id: self.tracker_id,
-        role_id: roles.map{|r| r.id},
-        workspace_id: self.project.workspace_id
-      ).map{|w| w.new_status_id}.include?(self.issue_status_id)
+    self.project.present? &&
+    self.tracker.present? &&
+    self.project.trackers.include?(self.tracker) &&
+    self.issue_status.present? &&
+    roles.map{|role| can_add_issue(role)}.any? &&
+    WorkflowTransition.where(
+      old_status_id: 0,
+      tracker_id: self.tracker_id,
+      role_id: roles.map{|r| r.id},
+      workspace_id: self.project.workspace_id
+    ).map{|w| w.new_status_id}.include?(self.issue_status_id)
   end
 
   private
